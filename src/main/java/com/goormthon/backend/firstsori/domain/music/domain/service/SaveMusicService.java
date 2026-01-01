4b4881cf86db4c271e2915cc72e353f36aba160e
Choi Min Young <codingmy@naver.com>
2026-01-02 02:31:32 +0900

    [hotfix/#57] 동시성 상황에서 중복 처리 방지를 위한 로직 개선

--- src/main/java/com/goormthon/backend/firstsori/domain/music/domain/service/SaveMusicService.java
+++ src/main/java/com/goormthon/backend/firstsori/domain/music/domain/service/SaveMusicService.java
@@ -2,14 +2,15 @@ package com.goormthon.backend.firstsori.domain.music.domain.service;
 
 import com.goormthon.backend.firstsori.domain.music.domain.entity.Music;
 import com.goormthon.backend.firstsori.domain.music.domain.repository.MusicRepository;
+import com.goormthon.backend.firstsori.global.common.exception.ErrorCode;
+import com.goormthon.backend.firstsori.global.common.response.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronizationAdapter;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
@@ -22,38 +23,42 @@ public class SaveMusicService {
     private final MusicAsyncService musicAsyncService;
 
     public Music saveMusic(Music music) {
-        Optional<Music> existsMusic = musicRepository.findBySongNameAndArtist(music.getSongName(), music.getArtist());
         Music targetMusic;
 
-        if (existsMusic.isEmpty()) {
+        try {
             targetMusic = musicRepository.save(music);
             log.info("DB에 새로운 음악 저장 완료: 곡='{}'", targetMusic.getSongName());
 
-        } else {
-            targetMusic = existsMusic.get();
-            log.info("음악이 이미 존재함: 곡='{}'",  targetMusic.getSongName());
+        } catch (DataIntegrityViolationException e) {
+            targetMusic = musicRepository
+                    .findBySongNameAndArtist(music.getSongName(), music.getArtist())
+                    .orElseThrow(() -> new CustomException(ErrorCode.MUSIC_CONSISTENCY_ERROR));
+            ;
 
+            log.info("이미 존재하는 음악 사용: 곡='{}'", targetMusic.getSongName());
         }
 
-        // 2. 트랜잭션 커밋 성공 시 비동기 작업 등록
-        // DB 변경사항이 Redis 및 Stream에 반영되기 전에 실패하는 것을 방지
-        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
-            @Override
-            public void afterCommit() {
-                log.info("✅ 트랜잭션 커밋 성공. 비동기 이벤트 및 캐시 TTL 연장 작업 시작.");
+        registerAfterCommit(targetMusic);
 
-                // 2.1. 검색 캐시 TTL 연장 (비동기)
-                musicAsyncService.extendSearchCacheTtl(targetMusic.getSongName());
-
-                // 2.2. 인기 차트 반영 이벤트 발행 (Stream)
-                musicEventProducer.publishMusicEvent(targetMusic.getSongName());
-            }
-        });
-
-        // 3. 결과 반환 (트랜잭션 커밋은 이 메서드 종료 후 Spring에 의해 실행)
         return targetMusic;
     }
 
+    // 2. 트랜잭션 커밋 성공 시 비동기 작업 등록
+    // DB 변경사항이 Redis 및 Stream에 반영되기 전에 실패하는 것을 방지
+    private void registerAfterCommit(Music music) {
+        TransactionSynchronizationManager.registerSynchronization(
+                new TransactionSynchronizationAdapter() {
+                    @Override
+                    public void afterCommit() {
+                        log.info("✅ 트랜잭션 커밋 성공. 비동기 이벤트 및 캐시 TTL 연장 작업 시작.");
+                        // 2.1. 검색 캐시 TTL 연장 (비동기)
+                        musicAsyncService.extendSearchCacheTtl(music.getSongName());
 
+                        // 2.2. 인기 차트 반영 이벤트 발행 (Stream)
+                        musicEventProducer.publishMusicEvent(music.getSongName());
+                    }
+                }
+        );
+    }
 
 }
